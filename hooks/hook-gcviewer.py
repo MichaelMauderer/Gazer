eed03e693d59b99535355e1ab1f5a9162452e2b4
Michael Mauderer <mail@michaelmauderer.com>
2015-10-12 16:17:35 +0100

    Fixed compilation process for scipy. Moved hidden import from script to hook file.

--- hooks/hook-gcviewer.py
+++ hooks/hook-gcviewer.py
@@ -1,3 +1,12 @@
+from PyInstaller.utils.hooks import collect_submodules
+
 datas = [
     ('.\\lib\\Tobii.EyeX.Client.dll', '.\\'),
 ]
+
+hiddenimports = ['gcviewer',
+                 'scipy',
+                 'skimage.io',
+                 ]
+
+hiddenimports += collect_submodules('scipy')
--- scripts/make_exe.py
+++ scripts/make_exe.py
@@ -7,10 +7,6 @@ import os
 
 from subprocess import check_output
 
-hidden_imports = [
-    'gcviewer'
-]
-
 project_root = '.\\'
 pyinstaller_path = os.getenv('PYINSTALLER_PATH', 'pyinstaller')
 lib_path = '.\\lib'
@@ -24,8 +20,6 @@ opts = '--clean ' \
        '--paths={lib_path} ' \
        '--additional-hooks-dir={hook_path} ' \
        '-y '
-
-opts += ' '.join(['--hidden-import=' + s for s in hidden_imports])
 
 opts = opts.format(project_root=project_root,
                    lib_path=lib_path,
