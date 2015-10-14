c27c7ed0f5ce1172747c411de8382d02cf3b8c80
David Morrison <dm236@st-andrews.ac.uk>
2015-10-14 14:57:25 +0100

    Imported Lytro tools module and used it to load both ifp and ipr files.

--- gcviewer/modules/dof/lytro_import.py
+++ gcviewer/modules/dof/lytro_import.py
@@ -1,3 +1,8 @@
-def read_ifr(file_name):
-    print('Loading IFR file: ' + file_name)
+import lpt.lfp.lfp as lfp
+
+
+def read_ifp(file_name):
+    print('Loading IFP or IFR file: ' + file_name)
+    loaded = lfp.Lfp(path=file_name)
+    print(loaded)
     print('Finished Loading.')
