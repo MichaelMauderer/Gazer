ff73c2c68416e005ec5d89757de0aacaf33822d3
Michael Mauderer <mail@michaelmauderer.com>
2015-10-16 10:37:18 +0100

    Added test for gaze coordinate conversion.

---
+++ tests/test_gui.py
@@ -0,0 +1,60 @@
+"""
+Defines unit tests for :mod:`gcviewer.gui` module.
+"""
+
+from __future__ import division, unicode_literals, print_function
+
+import sys
+import unittest
+
+import mock
+import numpy as np
+
+from PyQt4.QtGui import QApplication
+import PyQt4.QtCore as qt
+
+from gcviewer.gui import GCImageWidget
+
+app = QApplication(sys.argv)
+
+
+class TestCoordinateConversion(unittest.TestCase):
+    def setUp(self):
+        self.mock_scene = mock.MagicMock()
+        self.mock_scene.get_image = mock.MagicMock(return_value=np.zeros([100,
+                                                                          100]))
+        self.mock_scene.update_gaze = mock.MagicMock()
+
+        self.gc_image_widget = GCImageWidget(self.mock_scene)
+
+    def test_pixmap_resizing(self):
+        self.gc_image_widget.setFixedSize(1000, 2000)
+        pixmap = self.gc_image_widget.get_scaled_pixmap()
+        self.assertEqual(pixmap.size(), qt.QSize(1000, 1000))
+
+        self.gc_image_widget.setFixedSize(3000, 1000)
+        pixmap = self.gc_image_widget.get_scaled_pixmap()
+        self.assertEqual(pixmap.size(), qt.QSize(1000, 1000))
+
+        self.gc_image_widget.setFixedSize(500, 1000)
+        pixmap = self.gc_image_widget.get_scaled_pixmap()
+        self.assertEqual(pixmap.size(), qt.QSize(500, 500))
+
+        self.gc_image_widget.setFixedSize(500, 10)
+        pixmap = self.gc_image_widget.get_scaled_pixmap()
+        self.assertEqual(pixmap.size(), qt.QSize(10, 10))
+
+    def test_local_to_pixmap_conversion(self):
+        self.gc_image_widget.setFixedSize(1000, 2000)
+        self.gc_image_widget.setPixmap(self.gc_image_widget.get_scaled_pixmap())
+
+        convert = self.gc_image_widget.local_to_image_norm_coordinates
+
+        np.testing.assert_allclose(convert((0, 0)), (0, 0))
+        np.testing.assert_allclose(convert((1000, 1000)), (1.0, 1.0))
+
+        np.testing.assert_allclose(convert((500, 1000)), (0.5, 1.0))
+        np.testing.assert_allclose(convert((250, 1500)), (0.25, 1.0))
+
+        np.testing.assert_allclose(convert((1000, 500)), (1.0, 0.5))
+        np.testing.assert_allclose(convert((1500, 250)), (1.0, 0.25))
