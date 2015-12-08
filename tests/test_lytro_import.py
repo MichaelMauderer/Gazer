6ae992db30468bb62f56cae60a86b2399a03f7f9
Michael Mauderer <mm285@st-andrews.ac.uk>
2015-12-08 11:02:25 +0000

    Added more remap test cases.

--- tests/test_lytro_import.py
+++ tests/test_lytro_import.py
@@ -5,21 +5,21 @@ from gcviewer.modules.dof.lytro_import import value_map_to_index_map, remap
 def test_value_map_to_index_map():
     index_list = [21, 22, 24, 128]
     value_map = np.array(
-        [
-            [21, 22, 24, 128],
-            [21, 22, 24, 128],
-            [21, 22, 24, 128],
-            [21, 22, 24, 128],
-        ]
+            [
+                [21, 22, 24, 128],
+                [21, 22, 24, 128],
+                [21, 22, 24, 128],
+                [21, 22, 24, 128],
+            ]
     )
     index_map = value_map_to_index_map(value_map, index_list)
     expected = np.array(
-        [
-            [0, 1, 2, 3],
-            [0, 1, 2, 3],
-            [0, 1, 2, 3],
-            [0, 1, 2, 3],
-        ]
+            [
+                [0, 1, 2, 3],
+                [0, 1, 2, 3],
+                [0, 1, 2, 3],
+                [0, 1, 2, 3],
+            ]
     )
     np.testing.assert_array_equal(index_map, expected)
 
@@ -27,25 +27,22 @@ def test_value_map_to_index_map():
 def test_value_map_to_index_map_simple():
     index_list = [0, 255]
     value_map = np.array(
-        [
-            [0, 123],
-            [234, 255]
-        ]
+            [
+                [0, 123],
+                [234, 255]
+            ]
     )
     index_map = value_map_to_index_map(value_map, index_list)
     expected = np.array(
-        [
-            [0, 0],
-            [1, 1],
-        ]
+            [
+                [0, 0],
+                [1, 1],
+            ]
     )
     np.testing.assert_array_equal(index_map, expected)
 
 
 def test_remap():
-    from_range = 0, 1
-    to_range = 10, 20
-    mapped = remap(0.5, from_range, to_range)
-    np.testing.assert_allclose(mapped, 15)
-
-
+    np.testing.assert_allclose(remap(0.5, (0, 1), (10, 20)), [15])
+    np.testing.assert_allclose(remap(0.5, (0, 1), (-10, 10)), [0])
+    np.testing.assert_allclose(remap(0.5, (-1, 1), (10, 20)), [17.5])
