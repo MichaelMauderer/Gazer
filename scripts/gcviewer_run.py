1c6fd56e8163a4c1e619c5b280196f631c7323c9
Michael Mauderer <mm285@st-andrews.ac.uk>
2016-01-14 10:32:35 +0000

    Improved logging.

--- scripts/gcviewer_run.py
+++ scripts/gcviewer_run.py
@@ -2,15 +2,26 @@ from __future__ import unicode_literals, division, print_function
 
 import logging
 import sys
+import os
 
 from PyQt4 import QtGui
 
 import gcviewer.eyetracking.api
 from gcviewer.qt_gui.mainwindow import GCImageViewerMainWindow
 
-logging.basicConfig(filename='log.debug', level=logging.DEBUG)
+DEBUG_LOG_FILE = 'debug.log'
+logging.basicConfig(filename=DEBUG_LOG_FILE, level=logging.DEBUG, filemode='w')
 
 logger = logging.getLogger(__name__)
+
+
+def log_heading():
+    logging.info('*' * 70)
+    logging.info('*' * 3 + ' In case of problems with this applications please'
+                           ' provide the  ***')
+    logging.info(
+            '*** content of this file for diagnostics.' + ' ' * 26 + '*' * 3)
+    logging.info('*' * 70)
 
 
 def run_qt_gui():
@@ -18,17 +29,19 @@ def run_qt_gui():
     Set up example configuration to run qt gui with eyex and save log files.
     """
 
+    log_heading()
+
     app = QtGui.QApplication(sys.argv)
     tracking_apis = gcviewer.eyetracking.api.get_available()
-    logger.debug(
-        'Available tracking apis: {}'.format(str(tracking_apis.keys())))
-    imageViewer = GCImageViewerMainWindow(tracking_apis)
-    imageViewer.show()
+    logger.info(
+            'Available tracking apis: {}'.format(str(tracking_apis.keys())))
+    imageviewer = GCImageViewerMainWindow(tracking_apis)
+    imageviewer.show()
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     try:
         run_qt_gui()
-    except Exception:
+    except RuntimeError:
         logging.exception('Program terminated with an exception. ')
--- scripts/make_exe.py
+++ scripts/make_exe.py
@@ -7,6 +7,7 @@ import os
 
 from subprocess import check_output
 
+app_name = 'gcviewer'
 project_root = '..'
 pyinstaller_path = os.getenv('PYINSTALLER_PATH', 'pyinstaller')
 lib_path = os.path.join('..', 'lib')
@@ -23,13 +24,15 @@ opts = '--clean ' \
        '--additional-hooks-dir={hook_path} ' \
        '-y ' \
        '--distpath={out_path} ' \
-       '--workpath={build_path} '
+       '--workpath={build_path} ' \
+       '--name {app_name}'
 
 opts = opts.format(project_root=project_root,
                    lib_path=lib_path,
                    hook_path=hook_path,
                    out_path=out_path,
-                   build_path=build_path
+                   build_path=build_path,
+                   app_name=app_name,
                    )
 
 command = pyinstaller_path + ' ' + opts + ' ' + target
