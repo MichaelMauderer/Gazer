bc5e5bcdfb30244ffb2981d712406e4e2362e9f2
Michael Mauderer <mm285@st-andrews.ac.uk>
2016-01-29 10:24:34 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='Gazer',
-        version='0.1-alpha.2',
+        version='0.1-alpha.3',
         packages=['gazer', 'gazer.qt_gui', 'gazer.modules', 'gazer.modules.dof',
                   'gazer.eyetracking'],
         url='',
